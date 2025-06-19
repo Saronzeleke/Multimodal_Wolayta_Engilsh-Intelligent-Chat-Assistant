fef2221d1273fc82ed454bb66b20f286a5a6d067
Saronzeleke <Cassiopiasaron@gmail.com>
2025-06-19 09:37:13 +0300

    commit

--- app/translation.py
+++ app/translation.py
@@ -1,3 +1,5 @@
 from tranformers import MarianMTModel,MarianTokenizer
 Model_name='Helsinki-NLP/opus-mt-en-de'
 tokinzer=MarianTokenizer.from_pretranined(Model_name)
+model=MarianMTModel.from_pretranined(Model_name)
+def Translate(text:str,source_lang:str="en",target_lang:str="wo")
