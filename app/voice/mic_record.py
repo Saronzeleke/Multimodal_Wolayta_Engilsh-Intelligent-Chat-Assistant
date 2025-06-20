45f3387f6f9f1afc999271b6073452add5faff3a
Saronzeleke <Cassiopiasaron@gmail.com>
2025-06-20 09:54:49 +0300

    commit

---
+++ app/voice/mic_record.py
@@ -0,0 +1,31 @@
+import pyaudio
+import wave
+
+def record_audio(output_file: str = "assets/recorded.wav", duration: int = 5, rate: int = 16000) -> None:
+    chunk = 1024  # Record in chunks of 1024 samples
+    format = pyaudio.paInt16  # 16-bit resolution
+    channels = 1  # Mono
+
+    audio = pyaudio.PyAudio()
+
+    print(f"[RECORDING] Recording {duration} seconds of audio...")
+    stream = audio.open(format=format, channels=channels,
+                        rate=rate, input=True,
+                        frames_per_buffer=chunk)
+
+    frames = []
+    for _ in range(0, int(rate / chunk * duration)):
+        data = stream.read(chunk)
+        frames.append(data)
+
+    print("[RECORDING] Done recording.")
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    with wave.open(output_file, 'wb') as wf:
+        wf.setnchannels(channels)
+        wf.setsampwidth(audio.get_sample_size(format))
+        wf.setframerate(rate)
+        wf.writeframes(b''.join(frames))
+    print(f"[RECORDING] Saved to {output_file}")
