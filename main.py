1cb515d50962981db4c2ce6e6ce54d42a23292b2
Saronzeleke <Cassiopiasaron@gmail.com>
2025-06-20 09:18:44 +0300

    commit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from app.translation.translation import translate
-
-# Try translating from English → Amharic as a placeholder
 source_text = "How are you today?"
 translated = translate(source_text, source_lang="en", target_lang="am")
 
