16aeb75a0d4f4ce18f30c2c05d683cb3c35d950b
Saronzeleke <Cassiopiasaron@gmail.com>
2025-06-20 10:00:44 +0300

    commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from app.translation.translation import translate
 from app.voice.pipline import voice_translate_pipeline
 from app.voice.mic_record import record_audio
+import os
 source_text = "How are you today?"
 translated = translate(source_text, source_lang="en", target_lang="am")
 
@@ -8,7 +9,8 @@ print(f"Original: {source_text}")
 print(f"Translated: {translated}")
 
 if __name__ == "__main__":
-    import os
+   
+    record_audio("assets/recorded.wav", duration=5)
     input_audio = "assets/recorded.wav"
     if not os.path.exists(input_audio):
         print(f"[ERROR] File not found: {input_audio}")
