c3b75462598e762c930f9c373c16cb6ab04d3545
Saronzeleke <Cassiopiasaron@gmail.com>
2025-06-27 09:41:50 +0300

    commit

--- app/QA/qa.py
+++ app/QA/qa.py
@@ -1,7 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))  # 👈 Add early!
-
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))) 
 import pandas as pd
 import torch
 import faiss
