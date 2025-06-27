41886aa15dfd59f0b559508022c08f7c612b73d8
Saronzeleke <Cassiopiasaron@gmail.com>
2025-06-27 09:29:06 +0300

    commit

--- summery_run_api.py
+++ summery_run_api.py
@@ -15,4 +15,4 @@ def read_root():
     return {"message": "✅ Wolaytta Chat Assistant API is running."}
 
 if __name__ == "__main__":
-    uvicorn.run("run_api:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("summery_run_api:app", host="127.0.0.1", port=8000, reload=True)
