e65ea5a46a5c786e9b4b510d36367410c61006c5
Saronzeleke <Cassiopiasaron@gmail.com>
2025-06-27 11:03:47 +0300

    commit

--- auto_label_wolayta_sentiment.py
+++ auto_label_wolayta_sentiment.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 INPUT_CSV =r"C:\Users\admin\Desktop\Multimodal_Wolayta_Engilsh-Intelligent-Chat-Assistant\data\wolayta.csv"
-OUTPUT_CSV =r"C:\Users\admin\Multimodal_Wolayta_Engilsh-Intelligent-Chat-Assistant\data\wolayta_sentiment_labeled.csv"
+OUTPUT_CSV =r":\Users\admin\Desktop\Multimodal_Wolayta_Engilsh-Intelligent-Chat-Assistant\data\wolayta_sentiment_labeled.csv"
 
 def main():
     logger.info(f"Loading dataset from {INPUT_CSV}")
