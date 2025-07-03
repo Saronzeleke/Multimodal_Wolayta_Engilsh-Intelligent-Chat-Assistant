033364e4caf6c702623b96aeeba08b0e7e05288d
Saronzeleke <Cassiopiasaron@gmail.com>
2025-07-03 09:51:29 +0300

    commit

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from app.translation.translation import translate
 from app.voice.pipline import voice_translate_pipeline
 from app.voice.mic_record import record_audio
 from app.QA.qa import router as qa
-from app.translation.translation_api import router as translation_router
+from app.translation.translation_api import translate_endpoint
 from app.routes.summarizer_api import router as summarizer_router
 # You can import others like sentiment or voice here too
 import os
 source_text = "How are you today?"
-translated = translate(source_text, source_lang="en", target_lang="am")
+translated = translate(source_text, source_lang="en", target_lang="wolaytta")
 
 print(f"Original: {source_text}")
 print(f"Translated: {translated}")
@@ -27,7 +27,7 @@ app.add_middleware(
 
 # Include API routers
 app.include_router(qa, prefix="/api/qa", tags=["Question Answering"])
-app.include_router(translation_router, prefix="/api/translate", tags=["Translation"])
+app.include_router(translate_endpoint, prefix="/api/translate", tags=["Translation"])
 app.include_router(summarizer_router, prefix="/api/summarize", tags=["Summarization"])
 # Future: app.include_router(voice_router, prefix="/api/voice", tags=["Voice Input/Output"])
 
